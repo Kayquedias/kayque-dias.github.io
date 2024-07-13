11478f4c092a60d573be84aa6014665470c6bc17
Kayquedias <kayquedias204@gmail.com>
2024-07-13 18:32:23 -0300

    update: with personal data

--- src/data/mydata.ts
+++ src/data/mydata.ts
@@ -1,4 +1,4 @@
 export const RATE_PER_HOUR = 128_788.05; // $50.00
-export const GITHUB_USERNAME = "katongole-isaac";
-export const GITHUB_REPO = "katongole-isaac.github.io";
+export const GITHUB_USERNAME = "Kayquedias";
+export const GITHUB_REPO = "Kayquedias.github.io";
 
